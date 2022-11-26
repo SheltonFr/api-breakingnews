0dc5ac6a430a77dfd3489fbe822aef76a2fbd0bf
Shelton Francisco <sheltonfjs@gmail.com>
2022-11-26 11:56:50 +0200

    User: Criptograando password com Bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 // configurando a o documetn do user ("""""""Tabela""""""")
 const UserSchema = new mongoose.Schema({
@@ -14,10 +15,12 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        // lowercase: true,
     },
     password: {
         type: String,
         required: true,
+        select: true, // para que na seja retornada nas consultas a base de dados
     },
     avatar: {
         type: String,
@@ -28,6 +31,21 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
 });
+
+/* Criptografia de password */
+
+// pre() -> funcao a ser executada antes de se armazenar o registro
+UserSchema.pre('save', async function (next) {
+    /* o argumento numerico representa o numero de rodadas de hash. remomenda-se que nao seja inferior a 10 */
+    this.password = await bcrypt.hash(this.password, 10);
+
+
+    /*  A Criptografia leve certo tempo, porem é necessario que ela termine, para poder se efectuar 
+    a persistencia do registro na base de dados. ou seja, é uma funcao assincrona, que deve ser concluida
+    antes de se realizar o proimo passo(salvar)*/
+
+    next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
