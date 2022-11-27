be200aa8c67675572772c1f21e80672bc759733f
Shelton Francisco <sheltonfjs@gmail.com>
2022-11-27 09:50:52 +0200

    Refactor: passando o id como parametro a partir da middlware validId

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -7,16 +7,18 @@ export const validId = (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(400).send({ message: "Invalid id" });
 
+  // envia o id valido, como paramtro directo da requisicao. A funcao next() podera acessar directamente do req.id
+  req.id = id;
+
   next();
 };
 
 export const validUser = async (req, res, next) => {
-  const id = req.params.id;
+  const id = req.id;
   const user = await userService.findById(id);
   if (!user) return res.status(400).send({ message: `User ${id} not found` });
 
   // envia os parametros como requisicao para a funcao next
-  req.id = id;
   req.user = user;
   next();
 };
