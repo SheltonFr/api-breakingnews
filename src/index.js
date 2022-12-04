a28befe41ac8fb106ecc0d7d24c4d1f1f55bb7f1
Shelton Francisco <sheltonfjs@gmail.com>
2022-12-04 19:58:12 +0200

    removendo swagger

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv"
 import userRoute from "./routes/user.rout.js";
 import authRouter from "./routes/auth.rout.js";
 import newsRouter from "./routes/news.route.js";
-import swaggerRouter from "./routes/swagger.rout.js"
+// import swaggerRouter from "./routes/swagger.rout.js"
 
 
 dotenv.config(); // habilita variaveis de ambiente
@@ -22,6 +22,6 @@ app.use(express.json()); // permite que sejam feitas requisicaoes reebendo JSON
 app.use("/users", userRoute);
 app.use("/auth", authRouter);
 app.use("/news", newsRouter);
-app.use("/doc", swaggerRouter)
+// app.use("/doc", swaggerRouter)
 
 app.listen(port, () => console.log(`Server runnign on port: ${port}`));
